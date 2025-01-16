8d2649e182b19b4aec1e15675772be2f60132329
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2025-01-16 10:55:49 +0530

    Environment variables are fixed in CSR. - .prettierrc is added.

--- frontend/src/utilites/config.ts
+++ frontend/src/utilites/config.ts
@@ -4,10 +4,11 @@ import process from "process";
 
 export const getConfig = (key: ConfigKeys): string | undefined => {
     if (isSsr()) {
-        const serverEnv = typeof process !== "undefined" && process.env ? process.env : {};
+        const serverEnv =
+            typeof process !== "undefined" && process.env ? process.env : {};
         return serverEnv[key] as string | undefined;
     }
 
-    const clientEnv = typeof window !== "undefined" && window.hievents ? window.hievents : {};
+    const clientEnv = import.meta.env;
     return clientEnv[key];
 };
